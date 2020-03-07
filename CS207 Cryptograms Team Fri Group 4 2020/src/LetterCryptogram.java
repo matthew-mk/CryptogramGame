3316aed32e9dd203be8ac5dc771e5ce208535b76
Matthew King CS2018 <matthew.king.2018@uni.strath.ac.uk>
2020-03-07 15:47:11 +0000

    Added a missing ;

--- CS207 Cryptograms Team Fri Group 4 2020/src/LetterCryptogram.java
+++ CS207 Cryptograms Team Fri Group 4 2020/src/LetterCryptogram.java
@@ -2,7 +2,7 @@ import java.util.HashMap;
 
 public class LetterCryptogram extends Cryptogram {
 	
-	public HashMap<String, String> cryptogramAlphabet
+	public HashMap<String, String> cryptogramAlphabet;
 	
 	
 	
