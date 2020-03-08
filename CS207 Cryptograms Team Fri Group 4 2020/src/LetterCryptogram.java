c5b08581e81f1e5cbc26c3062409736904605e1c
Craig McKenna CSM2018 <craig.mckenna.2018@uni.strath.ac.uk>
2020-03-08 21:31:44 +0000

    Created matchLetterToLetter() method

--- CS207 Cryptograms Team Fri Group 4 2020/src/LetterCryptogram.java
+++ CS207 Cryptograms Team Fri Group 4 2020/src/LetterCryptogram.java
@@ -1,39 +1,56 @@
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
+import java.util.*;
 
 public class LetterCryptogram extends Cryptogram {
-	
-	public void Cryptogram(String file) {
-		
+
+	public LetterCryptogram(String file) {
+		this.phrase = file;
+		this.userGuess = new HashMap<>();
+		matchLetterToLetter();
+
+		this.encryptedPhrase = new ArrayList<>();
+		for (Character c: phrase.toCharArray()) {
+			String out = cryptoMapping.get(c.toString());
+			if (out != null) {
+				encryptedPhrase.add(out);
+			} else {
+				encryptedPhrase.add(c.toString());
+			}
+			encryptedPhrase.add(" ");
+		}
 	}
-	
-	public void Cryptogram() {
-		
-	}
-	
+
 	/*
 	public char getPlainLetter(char cryptoLetter) {
 		
 	}
 	*/
 
+	public void matchLetterToLetter() {
+		this.cryptoMapping = new HashMap<>();
+		this.answerMapping = new HashMap<>();
 
-	@Override
-	public void addLetter(String c, String n) {
+		HashSet<String> unique = new HashSet<>();
+		for (Character c: phrase.toCharArray()) {
+			if (Character.isDigit(c) || Character.isLetter(c)) {
+				unique.add(c.toString());
+			}
+		}
 
+		String[] letterArray = new String[26];
+		for (char ch = 'a'; ch <= 'z'; ch++)
+		{
+			letterArray[ch - 'a'] = String.valueOf(ch);
+		}
+		Collections.shuffle(Arrays.asList(letterArray));
+
+		int i = 0;
+		for (String c: unique) {
+			cryptoMapping.put(c, letterArray[i]);
+			answerMapping.put(letterArray[i], c);
+			i++;
+		}
 	}
 
-	@Override
-	public void undoLetter(String c) {
-
-	}
-
-	@Override
-	public boolean isSolved() {
-		return false;
-	}
-	
 	/*
 	public void undoLetter(char c) {
 		for (int i = 0; i < getPhraseLength(); i++) {
