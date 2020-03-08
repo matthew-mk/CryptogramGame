cd14ec9f38fe1111cd37c985190fe58e8dfa2299
Craig McKenna CSM2018 <craig.mckenna.2018@uni.strath.ac.uk>
2020-03-08 21:34:33 +0000

    Added ability to choose the type of cryptogram

--- CS207 Cryptograms Team Fri Group 4 2020/src/Main.java
+++ CS207 Cryptograms Team Fri Group 4 2020/src/Main.java
@@ -1,9 +1,28 @@
+import java.util.*;
 
 public class Main {
 	
 	public static void main(String[] args) {
-		
-		Game game = new Game("player", "NumberCryptogram");
+
+		Scanner input = new Scanner(System.in);
+		System.out.println("Do you want to play a \"Number\" or a \"Letter\" cryptogram?");
+		String cType;
+		while(true){
+			cType = input.next();
+			if (cType.equals("Number")){
+				cType = "NumberCryptogram";
+				break;
+			}
+			else if (cType.equals("Letter")){
+				cType = "LetterCryptogram";
+				break;
+			}
+			else{
+				System.out.println("You have entered an invalid type.");
+			}
+		}
+
+		Game game = new Game("player", cType);
 		game.run();
 	}
 	
