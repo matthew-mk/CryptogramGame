1234f8d811cd1ea322eac94a18f20ee96e6624e1
Craig McKenna CSM2018 <craig.mckenna.2018@uni.strath.ac.uk>
2020-03-20 19:56:21 +0000

    Added username getter

--- CS207 Cryptograms Team Fri Group 4 2020/src/Player.java
+++ CS207 Cryptograms Team Fri Group 4 2020/src/Player.java
@@ -1,54 +1,57 @@
 public class Player {
-	
+
 	public String username;
 	public int accuracy;
 	public int accurateGuesses;
 	public int totalGuesses;
 	public int cryptogramsPlayed;
 	public int cryptogramsCompleted;
-	
-	
+
+
 	public Player(String username) {
-	    this.accuracy = 0;
-	    this.accurateGuesses = 0;
-	    this.totalGuesses = 0;
-	    this.cryptogramsPlayed = 0;
-	    this.cryptogramsCompleted = 0;
+		this.accuracy = 0;
+		this.accurateGuesses = 0;
+		this.totalGuesses = 0;
+		this.cryptogramsPlayed = 0;
+		this.cryptogramsCompleted = 0;
 		this.username = username;
 	}
-	
-	
+
+
 	public void updateAccuracy() {
 		accuracy = (accurateGuesses * 100 / totalGuesses);
 	}
-	
+
 	public void incrementCryptogramsCompleted() {
 		cryptogramsCompleted++;
 	}
-	
+
 	public void incrementCryptogramsPlayed() {
 		cryptogramsPlayed++;
 	}
-	
+
 	public int getAccuracy() {
 		return accuracy;
 	}
-	
+
 	public int getAccurateGuesses() {
 		return accurateGuesses;
 	}
-	
+
 	public int getTotalGuesses() {
 		return totalGuesses;
 	}
-	
+
 	public int getNumCryptogramsCompleted() {
 		return cryptogramsCompleted;
 	}
-	
+
 	public int getNumCryptogramsPlayed() {
 		return cryptogramsPlayed;
 	}
 
-}
+	public String getUsername(){
+		return this.username;
+	}
 
+}
